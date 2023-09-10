de822c49f668905f7d84215f49bdf5d6c57178df
Janos Konig <janos.koenig@gmail.com>
2023-09-10 09:57:25 +0200

    Added controls using screen.listen() method. Fixed the bug of letting the snake change direction more than 90 degrees by adding an if statement in the .up, .down, .left, .right methods.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
 import time
 
@@ -20,8 +20,6 @@ screen.tracer(0)
 #     snake.goto(x=0-(_*20),y=0)
 #     segments.append(snake)
 
-snake = Snake()
-
 # Step 2.1: Moving our snake
 # game_is_on = True
 # while game_is_on:
@@ -31,19 +29,36 @@ snake = Snake()
 #         seg.forward(20)
 
 # Step 2.2: linking the bodies
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
     # for seg_num in range(len(segments)-1, 0, -1):
     #     new_x = segments[seg_num-1].xcor()
     #     new_y = segments[seg_num-1].ycor()
     #     segments[seg_num].goto(new_x, new_y)
     # segments[0].forward(20)
+
+# Step 2.3: Transitioning to OOP
+snake = Snake()
+# Step 3: Control the snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     snake.move()
 
-# Step 3:
+
 # Step 4:
+
+
 # Step 5:
+
+
 # Step 6:
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 class Snake:
     """Models the snake's movements, lengthens the snake, and much more"""
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for _ in range(3):
@@ -20,3 +25,21 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+
